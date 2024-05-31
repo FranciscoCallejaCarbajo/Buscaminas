180f2c17105aaace1ab21909a5cc8cbc65925ffd
LanLop1 <lanchazolopezs@gmail.com>
2024-05-31 14:59:39 +0200

    holii

--- javaScript/bienvenida.js
+++ javaScript/bienvenida.js
@@ -20,4 +20,47 @@ function borrarTexto (elemento){
 // Ejecutar la funcion texto
 efecto(mensaje,texto);
 
+let coche1 = document.getElementById("coche");
+let autobus1 = document.getElementById("autobus");
+function movimiento (t=0, a=0, cc=0){
+    if (t<120){
+        coche1.style.marginLeft= (t*1.5) + `vw`;
+        setTimeout(() => movimiento(t + 1), 60);
+    }else if (a<120) {
+        autobus1.style.display= `flex`;
+        autobus1.style.marginLeft= a + `vw`;
+        setTimeout(() => movimiento(t + 1, a + 1), 60);
+    }
+}
+
+function invertirImagenCoche() {
+    
+    if (coche1.style.transform === "scaleX(1)") {
+      coche1.style.transform = "scaleX(-1)";
+    } else {
+      coche1.style.transform = "scaleX(1)";
+    }
+  }
+  function invertirImagenAutobus() {
+    
+    if (autobus1.style.transform === "scaleX(1)") {
+      autobus1.style.transform = "scaleX(-1)";
+    } else {
+      autobus1.style.transform = "scaleX(1)";
+    }
+  }
+function vuelta(){
+    let estiloscoche = getComputedStyle(coche1);
+    let estilosaut = getComputedStyle(autobus1);
+    let margautobus = estilosaut.marginLeft;
+    let margcoche = estiloscoche.marginLeft;
+    if(margautobus>0){
+        autobus1.style.marginLeft= margautobus + `vw`;
+        setTimeout(() => vuelta(margautobus-1), 60);
+    }
+
+}
+movimiento()
+invertirImagenAutobus()
+vuelta()
 
