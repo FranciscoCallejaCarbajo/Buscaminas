d75e420ef85a759230943fa9b4c62b18dfca33f8
LanLop1 <lanchazolopezs@gmail.com>
2024-05-31 18:03:00 +0200

    final

--- javaScript/bienvenida.js
+++ javaScript/bienvenida.js
@@ -22,7 +22,7 @@ efecto(mensaje,texto);
 
 let coche1 = document.getElementById("coche");
 let autobus1 = document.getElementById("autobus");
-function movimiento (t=0, a=0, cc=0){
+function movimiento (t=-20, a=-20){
     if (t<120){
         coche1.style.marginLeft= (t*1.5) + `vw`;
         setTimeout(() => movimiento(t + 1), 60);
@@ -30,40 +30,36 @@ function movimiento (t=0, a=0, cc=0){
         autobus1.style.display= `flex`;
         autobus1.style.marginLeft= a + `vw`;
         setTimeout(() => movimiento(t + 1, a + 1), 60);
-    }
+        
+    }else {invertirImagenAutobus(), vuelta()}
 }
 
-function invertirImagenCoche() {
-    
-    if (coche1.style.transform === "scaleX(1)") {
-      coche1.style.transform = "scaleX(-1)";
-    } else {
-      coche1.style.transform = "scaleX(1)";
-    }
-  }
+
   function invertirImagenAutobus() {
-    
-    if (autobus1.style.transform === "scaleX(1)") {
-      autobus1.style.transform = "scaleX(-1)";
-    } else {
-      autobus1.style.transform = "scaleX(1)";
-    }
+      autobus1.style.transform = `scaleX(-1)`;
+      coche1.style.transform = `scaleX(-1)`;
   }
-function vuelta(){
-    let estiloscoche = getComputedStyle(coche1);
-    let estilosaut = getComputedStyle(autobus1);
-    let margautobus = estilosaut.marginLeft;
-    let margcoche = estiloscoche.marginLeft;
-    if(margautobus>0){
-        autobus1.style.marginLeft= margautobus + `vw`;
-        setTimeout(() => vuelta(margautobus-1), 60);
+  function vuelta (x=119, y=70){
+    if (x>=-25){
+        autobus1.style.marginLeft= (x) + `vw`;
+        setTimeout(() => vuelta(x - 1), 60);
+    }else{vueltacoche()}
     }
 
+function vueltacoche(y=119){{
+  coche1.style.marginLeft= (y)+ `vw`;
+  setTimeout(() => vueltacoche(y - 1), 60);
+  autobus1.style.display= `none`;
 }
-movimiento()
-invertirImagenAutobus()
-vuelta()
+}
 
+movimiento()
+
+
+
+// try{invertirImagenAutobus()}catch(error){console.error(error)}
+
+// try{vuelta()}catch(error){console.error("novuelve")}
 
 
 
